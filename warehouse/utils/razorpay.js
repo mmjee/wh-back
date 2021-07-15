52fe2eb21ae02a244727965e0a685e94bb687e85
root <root@git.maharshi.ninja>
2021-07-16 05:27:56 +0530

    Fix Razorpay

--- warehouse/utils/razorpay.js
+++ warehouse/utils/razorpay.js
@@ -1,8 +1,12 @@
 const Razorpay = require('razorpay')
 const ConfigManager = require('warehouse/utils/config')
 
-module.exports = new Razorpay({
-  key_id: ConfigManager.getKey('services.rzpId', process.env.RZP_KEY_ID),
-  key_secret: ConfigManager.getKey('services.rzpKey', process.env.RZP_KEY_SECRET)
-})
-global.Razorpay = module.exports
+const KID = ConfigManager.getKey('services.rzpId', process.env.RZP_KEY_ID)
+
+if (KID) {
+  module.exports = new Razorpay({
+    key_id: KID,
+    key_secret: ConfigManager.getKey('services.rzpKey', process.env.RZP_KEY_SECRET)
+  })
+  global.Razorpay = module.exports
+}
